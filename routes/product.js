59d4f52ab926f28a7b4eacc582bebd241270a0b9
Yogesh Aithani <aithaniyogesh007@gmail.com>
2025-03-24 12:05:22 +0530

    serach functionality added

--- routes/product.js
+++ routes/product.js
@@ -53,8 +53,12 @@ productRouter.get("/products", async (req, res) => {
 
 })
 
+
+
+
 productRouter.get("/products/random", async (req, res) => {
     try {
+
         const { limit = 10 } = req.query;
         const limitNumber = parseInt(limit, 12);
 
@@ -78,6 +82,61 @@ productRouter.get("/products/random", async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 })
+
+productRouter.get("/products/:search", async (req, res) => {
+    try {
+        const search = req.params.search;
+        const { page = 1, limit = 10, category, sort } = req.query;
+        const pageNumber = parseInt(page, 10);
+        const limitNumber = parseInt(limit, 10);
+        const skip = (pageNumber - 1) * limitNumber;
+        let filter = {}
+
+        if (category) {
+            filter.category = category;
+        }
+
+        if (search) {
+            const normalizedSearch = search
+                .toLowerCase()
+                .replace(/[\s\-]+/g, '.*');
+
+            filter.$or = [
+                { name: { $regex: normalizedSearch, $options: 'i' } },
+            ];
+        }
+
+        let sortOrder = {};
+        if (sort) {
+            if (sort === "price_asc") {
+                sortOrder.price = 1;
+            } else if (sort === 'price_desc') {
+                sortOrder.price = -1;
+            }
+        }
+
+        const products = await Product.find({ ...filter, isDeleted: false })
+            .sort(sortOrder)
+            .skip(skip)
+            .limit(limitNumber)
+            .select("_id name price images description");
+
+        const totalItems = await Product.countDocuments(filter);
+
+        res.status(200).json({
+            data: products,
+            pagination: {
+                currentPage: pageNumber,
+                totalPages: Math.ceil(totalItems / limitNumber),
+                totalProducts: totalItems,
+                pageSize: limitNumber,
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 productRouter.get("/product/:id", async (req, res) => {
     try {
